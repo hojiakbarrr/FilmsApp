7992041bf5c05bc1ad444a91be75990107938211
user <hojiakbar_1997@mail.ru>
2022-03-03 13:42:39 +0600

    ...

--- app/src/main/java/com/example/filmsapp/Movies_Activity.kt
+++ app/src/main/java/com/example/filmsapp/Movies_Activity.kt
@@ -1,8 +1,10 @@
 package com.example.filmsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -34,12 +36,13 @@ class Movies_Activity : AppCompatActivity() {
 
 
 
-        val apiInterface = ApiInterface.create().getMovies("1f49a5f345e0c857c3814334f71e360d")
+//        val apiInterface = ApiInterface.create().getMovies("1f49a5f345e0c857c3814334f71e360d")
+        val apiInterface = ApiInterface.create().getTopRatedMovies ("1f49a5f345e0c857c3814334f71e360d")
 
         //apiInterface.enqueue( Callback<List<Movie>>())
-        apiInterface.enqueue( object : Callback<Movies> {
-            override fun onResponse(call: Call<Movies>?, response: Response<Movies>?) {
-                val adapter = CustomAdapter(response?.body()?.results)
+        apiInterface.enqueue( object : Callback<Rating>, CustomAdapter.ItemClickListener {
+            override fun onResponse(call: Call<Rating>?, response: Response<Rating>?) {
+                val adapter = CustomAdapter(response?.body()?.results,this)
 
                 // Setting the Adapter with the recyclerview
                 recyclerview.adapter = adapter
@@ -48,10 +51,16 @@ class Movies_Activity : AppCompatActivity() {
 //                    recyclerAdapter.setMovieListItems(response.body()!!)
             }
 
-            override fun onFailure(call: Call<Movies>?, t: Throwable?) {
+            override fun onFailure(call: Call<Rating>?, t: Throwable?) {
                 Log.d("testLogs","OnResponse Success ${t?.message}")
 
             }
+
+            override fun onItemClick(id: Int) {
+                Toast.makeText(this@Movies_Activity,"click $id", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@Movies_Activity, MovieDetailsActivity::class.java)
+                intent.putExtra("id",id)
+                startActivity(intent)            }
         })
     }
 
